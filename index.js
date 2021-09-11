6b256662cb76741aeaf04cccceb576e80b0d7496
Garret Harp <32212214+garretcharp@users.noreply.github.com>
2021-09-11 01:23:33 +0000

    fix: remove console.log

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 const UpdateBuilder = require('./dynamo/updateBuilder')
 
 module.exports = { UpdateBuilder }
-
-console.log(
-  UpdateBuilder({
-    updates: {
-      set: [
-        ['something.here', 'is this'],
-        ['something.also', 'can be this']
-      ],
-      add: [
-        ['stats.updated', 10],
-        ['stats.viewed', 5]
-      ]
-    }
-  })
-)
